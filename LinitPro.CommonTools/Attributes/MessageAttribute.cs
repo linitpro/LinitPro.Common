dd81e63d18e5fcadbcb7e77edea5109b53e76b0f
u11703 <nkisselev@alfabank.kz>
2021-07-21 16:12:39 +0600

    initial

---
+++ LinitPro.CommonTools/Attributes/MessageAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LinitPro.CommonTools.Attributes
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class MessageAttribute: Attribute
+    {
+        public string Message { get; }
+
+        public MessageAttribute(string message)
+        {
+            this.Message = message;
+        }
+    }
+}
---
+++ LinitPro.CommonTools/Extensions/MessageAttributeExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using LinitPro.CommonTools.Attributes;
+
+namespace LinitPro.CommonTools.Extensions
+{
+    public static class MessageAttributeExtensions
+    {
+        public static string GetMessage(this object item)
+        {
+            var attributes = item.GetType().GetMember(item.ToString()).FirstOrDefault()
+                ?.GetCustomAttributes(typeof(MessageAttribute), false);
+            return attributes.Any() ? ((MessageAttribute)attributes[0]).Message : "";
+        }
+        
+        public static string GetMessage(this MemberInfo item)
+        {
+            var _attributes = item.GetCustomAttributes(typeof(MessageAttribute), false);
+            return _attributes.Any() ? ((MessageAttribute)_attributes[0]).Message : "";
+        }
+    }
+}
