38c11951fa2407ab0690733b147793727819fd4a
u11703 <nkisselev@alfabank.kz>
2021-07-21 16:37:34 +0600

    changes in nuget

--- LinitPro.CommonTools/obj/Debug/netstandard2.0/LinitPro.CommonTools.AssemblyInfo.cs
+++ LinitPro.CommonTools/obj/Debug/netstandard2.0/LinitPro.CommonTools.AssemblyInfo.cs
@@ -10,7 +10,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("LinitPro.CommonTools")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("linit.pro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
